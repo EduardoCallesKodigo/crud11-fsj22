666131b3866aa771da16e4b0d9118655509ec09c
Eduardo Calles <eduardo.calles@kodigo.org>
2024-10-14 17:54:46 -0600

    Primera parte

---
+++ resources/views/contacts/index.blade.php
@@ -0,0 +1,47 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Contact List') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6 lg:p-8">
+                <div class="bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
+                    
+                    <div class="mb-4">
+                        <a href="#"></a>
+                    </div>
+                    
+                    <table class="table-auto w-full">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NAME</th>
+                                <th>AGE</th>
+                                <th>EMAIL</th>
+                                <th>ACTIONS</th>
+                            </tr>
+                        </thead>
+
+
+                        <tbody>
+                            @foreach($contacts as $contact)
+                            <tr>
+                                <td>{{ $contact->id }}</td>
+                                <td>{{ $contact->name }}</td>
+                                <td>{{ $contact->age }}</td>
+                                <td>{{ $contact->email }}</td>
+                                <td>
+                                    dos botones
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
